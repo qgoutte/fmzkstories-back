3beac1cc6e35c8ab74020b4d9e4f8753355788d5
qgoutte <quentingoutte@gmail.com>
2023-08-22 11:44:15 +0200

    Inital commit 2

--- src/main/java/com/example/fmzkstoriesback/FmzkStoriesBackApplication.java
+++ src/main/java/com/example/fmzkstoriesback/FmzkStoriesBackApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FmzkStoriesBackApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FmzkStoriesBackApplication.class, args);
+       SpringApplication.run(FmzkStoriesBackApplication.class, args);
     }
 
 }
