dafbcca164c0f8da1a9c597c2bb7af21eaeeab5f
qgoutte <quentingoutte@gmail.com>
2023-08-22 11:40:37 +0200

    Inital commit

---
+++ src/main/java/com/example/fmzkstoriesback/FmzkStoriesBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.fmzkstoriesback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmzkStoriesBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FmzkStoriesBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fmzkstoriesback/FmzkStoriesBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fmzkstoriesback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmzkStoriesBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
